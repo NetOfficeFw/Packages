e8d09a2de49712dc2100fc84e9a2b574fdee6f4d
Jozef Izso <jozef.izso@gmail.com>
2020-06-28 17:57:00 +0200

    Improve NuGetPush.cmd

--- NuGetPush.cmd
+++ NuGetPush.cmd
@@ -23,8 +23,6 @@ set libs=(Core Access Excel MSFormsApi MSProject Outlook PowerPoint Publisher Vi
 for %%I in %libs% do (
   set library=%%I
   set file=NetOfficeFw.!library!.!_version!!_suffix!.nupkg
-  rem set symbols=NetOfficeFw.!library!.!_version!!_suffix!.symbols.nupkg
-  set symbols=NetOfficeFw.!library!.!_version!!_suffix!.snupkg
 
   echo NetOffice.!library!
   echo  push !file!
@@ -33,16 +31,7 @@ for %%I in %libs% do (
   REM https://api.nuget.org/v3/index.json
   nuget.exe push out\signed\!file! "!_token!" -NonInteractive -ForceEnglishOutput -Source https://api.nuget.org/v3/index.json
   if ERRORLEVEL 1 (
-    echo Failed to push to myget.org feed.
-  )
-
-  echo  push !symbols!
-  REM https://www.myget.org/F/netofficefw-dev/symbols/api/v2/package
-  REM https://www.myget.org/F/netoffice/symbols/api/v2/package
-  REM https://nuget.smbsrc.net/
-  nuget.exe push out\signed\!symbols! "!_token!" -NonInteractive -ForceEnglishOutput -Source https://api.nuget.org/v3/index.json
-  if ERRORLEVEL 1 (
-    echo Failed to push to myget.org symbols feed.
+    echo Failed to push to nuget.org feed.
   )
 
   echo.
