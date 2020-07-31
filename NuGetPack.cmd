3b1629a3a853491838644c0bff4c1e6dd3997377
Jozef Izso <jozef.izso@gmail.com>
2020-08-01 00:29:35 +0200

    NetOffice 1.7.5

--- NuGetPack.cmd
+++ NuGetPack.cmd
@@ -1,7 +1,7 @@
 @echo off
 setlocal EnableDelayedExpansion
 
-set _build=11
+set _build=0
 IF NOT "%APPVEYOR_BUILD_NUMBER%"=="" (
   set _build=%APPVEYOR_BUILD_NUMBER%
 )
@@ -10,7 +10,7 @@ IF NOT "%1"=="" (
 )
 
 set _suffix=
-set _version=1.7.4.%_build%%_suffix%
+set _version=1.7.5.%_build%%_suffix%
 set _configuration=Release
 IF NOT "%CONFIGURATION%"=="" (
   set _configuration=%CONFIGURATION%
